abb42d7c512d615194277877baaea99265e176be
Crapstur <romain.capouillez@free.fr>
2023-03-29 14:42:44 +0200

    TEST1

--- domain/repositories/repository_mongo.js
+++ domain/repositories/repository_mongo.js
@@ -9,12 +9,16 @@ const teamProjectManagementEvent = require('../entities/entity-team-project-mana
 let arrayConns = [], db = {};
 
 if (config.db.mongodb && config.db.mongodb.length > 0) {
-    config.db.mongodb.map((c)=>{
-        mongoose.connect(`mongodb://${c.host}/${c.database}`, {
+    config.db.mongodb.map((c) => {
+        mongoose.connect(`mongodb://haka47:Azerty0*@3f81311a-3a79-404d-8bb0-782a883b7837.hack48-5288.mongo.a.osc-fr1.scalingo-dbs.com:32380/hack48-5288?replicaSet=hack48-5288-rs0&ssl=true`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            maxPoolSize: 10 
-          });
+            maxPoolSize: 10
+        }).then(() => {
+            console.log('Connected to MongoDB');
+        }).catch((err) => {
+            console.error('Error connecting to MongoDB', err);
+        });
         db[c.nameconn] = {}
         db[c.nameconn].conn = mongoose;
         db[c.nameconn].User = user(mongoose);
@@ -23,6 +27,6 @@ if (config.db.mongodb && config.db.mongodb.length > 0) {
         db[c.nameconn].TeamProjectManagementEvent = teamProjectManagementEvent(mongoose);
     })
     exports.db = db;
-}else{
+} else {
     enum_.LogDanger("Erreur lors de la connexion à la db")
 }
