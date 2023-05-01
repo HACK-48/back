7f44863dc73a3e911a710e29b6fdb4fdc32afc51
Stephane0713 <67142299+Stephane0713@users.noreply.github.com>
2023-05-01 12:16:41 +0200

    Fix "OPTION" Request

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors          = require('cors');
 
 const allowCrossDomain = (req, res, next) => {
   res.header(`Access-Control-Allow-Origin`, `*`);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE, OPTION`);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE,OPTIONS`);
   res.header(`Access-Control-Allow-Headers`, "Origin, X-Requested-With, Content-Type, Accept");
   next();
 };
